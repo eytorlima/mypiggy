c4692fbed590efaa9d6baa0cc3e1640e568d9e49
eytorlima <eytorlima@outlook.com>
2026-07-07 22:06:43 -0300

    fix: minor changes

--- backend/src/main/java/com/mypiggy/model/Transaction.java
+++ backend/src/main/java/com/mypiggy/model/Transaction.java
@@ -1,8 +1,6 @@
 package com.mypiggy.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
